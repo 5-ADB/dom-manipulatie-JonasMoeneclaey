a2d4e5ef566fba66e833b9ec56b79416c020ce8b
jonas.moeneclaey <jonas.moeneclaey@student.staam.be>
2025-03-18 12:01:33 +0100

    1ste commit

--- script/script.js
+++ script/script.js
@@ -5,3 +5,27 @@ const gebruikers = [
   { id: 4, login: "jos.teugels", leeftijd: 13 },
   { id: 5, login: "piet.pollekes", leeftijd: 27 },
 ];
+const login = document.querySelector("#login")
+const leeftijd = document.querySelector("#leeftijd")
+const toevoegbtn = document.querySelector("#toevoegen")
+toevoegbtn.addEventListener("click", () => {
+  const idNummer = gebruikers.length + 1
+  gebruikers.push({id: idNummer, login: login.value, leeftijd: parseInt(leeftijd.value)})
+})
+
+const filterLeeftijd = document.querySelector("#filterLeeftijd")
+const ouder = document.querySelector("#ouder")
+const jonger = docuement.querySelector("#jonger")
+const lijstNamen = document.querySelector("#lijstNamen")
+
+ouder.addEventListener("click", () => {
+  gebruikers.forEach((namen) => {
+    lijstNamen.innerHTML = namen.login
+  })
+})
+jonger.addEventListener("click", () => {
+    gebruikers.forEach((namen) => {
+    lijstNamen.innerHTML = namen.login
+  })
+})
+
